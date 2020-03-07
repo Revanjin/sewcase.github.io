c366212f80f1748bfc3d4dce2ce9a4c03daec449
Revanjin <Revanjin@users.noreply.github.com>
2020-03-07 10:28:17 +0100

    Add files via upload
    Initial SetUp

---
+++ precache-manifest.7e540637f1360f282c5f09423af7eed6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6f25fa687536e01d3ffeb734f0eeb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "378e3cd9f49329b2a68b",
+    "url": "./static/css/main.9cd430e0.chunk.css"
+  },
+  {
+    "revision": "b865146e2d3f1260375b",
+    "url": "./static/js/2.69f60ce5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.69f60ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378e3cd9f49329b2a68b",
+    "url": "./static/js/main.1b323511.chunk.js"
+  },
+  {
+    "revision": "973ce5bfc1f931f651f7",
+    "url": "./static/js/runtime-main.2609201e.js"
+  },
+  {
+    "revision": "b6105e5a081195d8fba74ac08607aca6",
+    "url": "./static/media/first.b6105e5a.jpg"
+  },
+  {
+    "revision": "1c09de0a852b98cf6e34e8307935e42d",
+    "url": "./static/media/gamescom-team-edited.1c09de0a.jpg"
+  },
+  {
+    "revision": "3a68b2bd416ba2daf12afb492a3b7eaf",
+    "url": "./static/media/second.3a68b2bd.jpg"
+  }
+]);
