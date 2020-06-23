d69213a8f3a24faf2a5ff3386eb8022cb1f8ca36
tanachaithuerling <alexander.thuerling@netzkern.de>
2020-06-23 21:03:51 +0200

    updating styles for skills and base-layout

---
+++ precache-manifest.fb486ba45b2a80d1ab5ff948c38c34a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e835bc108e1c45c3a873eb52bf23025",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f41331e1235d05a93ca8",
+    "url": "./static/css/main.dbad3c30.chunk.css"
+  },
+  {
+    "revision": "900ebc5a4307d5df0c33",
+    "url": "./static/js/2.efdc95d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.efdc95d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41331e1235d05a93ca8",
+    "url": "./static/js/main.6de7a28c.chunk.js"
+  },
+  {
+    "revision": "ce14f66c8894118ce375",
+    "url": "./static/js/runtime-main.f92aead6.js"
+  },
+  {
+    "revision": "aac8372b285ada069aee8dbe1c5fcfaa",
+    "url": "./static/media/code.aac8372b.png"
+  },
+  {
+    "revision": "afc155f89dc18ad04394ab7928bfa8e3",
+    "url": "./static/media/dog.afc155f8.png"
+  },
+  {
+    "revision": "1afe2697cc199dfc8b397a02e167339d",
+    "url": "./static/media/info.1afe2697.png"
+  },
+  {
+    "revision": "4083f386a4eda0cb488f4dc525f82b8d",
+    "url": "./static/media/rocket.4083f386.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9c066af3f5511fd81bd7022032f5f880.js"
+  "./precache-manifest.fb486ba45b2a80d1ab5ff948c38c34a9.js"
 );
 
 self.addEventListener('message', (event) => {
