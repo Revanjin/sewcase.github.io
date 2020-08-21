1b1ba7edcca74890ba86f970714ca9d10191ace4
tanachaithuerling <alexander.thuerling@netzkern.de>
2020-08-21 17:18:34 +0200

    new page

---
+++ precache-manifest.016a263d0c9a1780e34f76ef8521120b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68dbc8a70889efee61ab34de06da536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "835a3d76ecae3916665f",
+    "url": "/static/css/main.88137f79.chunk.css"
+  },
+  {
+    "revision": "a141334ea1ea18d86d3e",
+    "url": "/static/js/2.125b14ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.125b14ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835a3d76ecae3916665f",
+    "url": "/static/js/main.54c55271.chunk.js"
+  },
+  {
+    "revision": "5aeda8031b04aeeb45c8",
+    "url": "/static/js/runtime-main.61926627.js"
+  },
+  {
+    "revision": "7a266f064e52e900297f15d02eb50766",
+    "url": "/static/media/169ahri.7a266f06.jpg"
+  },
+  {
+    "revision": "81cfd3153e13853d4fbf289eabea4ffb",
+    "url": "/static/media/169ekko.81cfd315.jpg"
+  },
+  {
+    "revision": "6899b1bf55992a4616aa20ed5cc95a9c",
+    "url": "/static/media/169pant.6899b1bf.jpg"
+  },
+  {
+    "revision": "e6770d5966ffe801f2c5cf61b8878120",
+    "url": "/static/media/Cassiopeia.e6770d59.jpg"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "980f29dbe42954c01002eeb87df15fbe",
+    "url": "/static/media/Sewcase_Logo_Black.980f29db.png"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "4d2c00fa3ae0549948481dd3a593fd17",
+    "url": "/static/media/ahri.4d2c00fa.jpg"
+  },
+  {
+    "revision": "c4183995ae25245143e1fb3a3fdb4864",
+    "url": "/static/media/arcade.c4183995.jpg"
+  },
+  {
+    "revision": "8248bee598ce43a08ab49a05cfa35db8",
+    "url": "/static/media/ez.8248bee5.jpg"
+  },
+  {
+    "revision": "44e718a905c1bd336a9caa09a91a1f74",
+    "url": "/static/media/fight.44e718a9.jpg"
+  },
+  {
+    "revision": "ef27f404ee2eb17c439073a21fa19df8",
+    "url": "/static/media/heimer.ef27f404.jpg"
+  },
+  {
+    "revision": "ce40aaf3c836c68ef45c356730777904",
+    "url": "/static/media/league.ce40aaf3.jpg"
+  },
+  {
+    "revision": "74a9491a3b26fece9075df287e23c40e",
+    "url": "/static/media/lul.74a9491a.jpg"
+  },
+  {
+    "revision": "d6d9e30293416f2aae6d0f7f7a1e7fd6",
+    "url": "/static/media/summonersrift.d6d9e302.jpg"
+  },
+  {
+    "revision": "9472277529e01a9aa321c47a08fa8d72",
+    "url": "/static/media/vijinx.94722775.jpg"
+  },
+  {
+    "revision": "f08189ee8973a348c4e19b64ff8db0d4",
+    "url": "/static/media/vijinxANDleague.f08189ee.jpg"
+  }
+]);
